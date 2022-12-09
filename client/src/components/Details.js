c12e99f1fe50bbddf37952ea9b74c542b1c8fc4a
samuel wolde <73776770+samuelwolde7@users.noreply.github.com>
2022-12-08 23:44:44 -0800

    Update Details.js

--- client/src/components/Details.js
+++ client/src/components/Details.js
@@ -23,7 +23,7 @@ const Details = () => {
 
     const getdata = async () => {
 
-        const res = await fetch(`https://crudappreactjs.herokuapp.com/getuser/${id}`, {
+        const res = await fetch(`https://backend-ysun.onrender.com/getuser/${id}`, {
             method: "GET",
             headers: {
                 "Content-Type": "application/json"
@@ -48,7 +48,7 @@ const Details = () => {
 
     const deleteuser = async (id) => {
 
-        const res2 = await fetch(`https://crudappreactjs.herokuapp.com/deleteuser/${id}`, {
+        const res2 = await fetch(`https://backend-ysun.onrender.com/deleteuser/${id}`, {
             method: "DELETE",
             headers: {
                 "Content-Type": "application/json"
